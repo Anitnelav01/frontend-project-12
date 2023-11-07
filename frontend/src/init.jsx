5113309c559d57cc686027e5444e961bfd87f9cc
root <root@LAPTOP-4T1UH53D>
2023-11-07 15:07:30 +0300

    Exploitation

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,6 +2,7 @@
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
 import resources from "./locales/resources.js";
 import { SocketContext } from "./contexts/index.jsx";
 import { api } from "./contexts/socketContext.js";
@@ -13,6 +14,10 @@ import store from "./slices/index.js";
 const init = () => {
   const censorshipDictionaryRu = leoProfanity.getDictionary('ru');
   leoProfanity.add(censorshipDictionaryRu);
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+    environment: "dev",
+  };
 
   i18n
     .use(initReactI18next)
@@ -25,13 +30,17 @@ const init = () => {
     });
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <SocketContext.Provider value={api}>
-          <App />
-        </SocketContext.Provider>
-      </I18nextProvider>
-    </Provider>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <SocketContext.Provider value={api}>
+            <App />
+          </SocketContext.Provider>
+        </I18nextProvider>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>
   );
 };
 
